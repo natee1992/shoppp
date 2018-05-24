3fc32b90618fc0b0ecbe5d7a1fa412e6542a9fd7
natee1992 <1092803008@qq.com>
2018-05-24 16:49:26 +0800

    new start

--- shoppp/settings.py
+++ shoppp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'shoppp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shoppp',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
     }
 }
 
